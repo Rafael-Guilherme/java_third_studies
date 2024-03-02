6bd0c95b586c8b1bf92d9dbec4e3bc12907e5c72
Rafael Guilherme <rafael_guilherme@hotmail.com>
2024-03-02 17:16:28 -0300

    feat: comparable treeset

--- src/ProgramProduct.java
+++ src/ProgramProduct.java
@@ -1,19 +1,19 @@
-import java.util.HashSet;
 import java.util.Set;
+import java.util.TreeSet;
 
 import entities.Product;
 
 public class ProgramProduct {
     public static void main(String[] args) {
 
-        Set<Product> set = new HashSet<>();
+        Set<Product> set = new TreeSet<>();
 
         set.add(new Product("TV", 900.00));
         set.add(new Product("Notebook", 1200.00));
         set.add(new Product("Tablet", 400.00));
 
-        Product prod = new Product("Notebook", 1200.00);
-
-        System.out.println(set.contains(prod));
+        for (Product p : set) {
+            System.out.println(p);
+        }
     }
 }
--- src/entities/Product.java
+++ src/entities/Product.java
@@ -28,12 +28,12 @@ public class Product implements Comparable<Product> {
 
     @Override
     public String toString() {
-        return name + ", " + String.format("%.2f", price);
+        return "Product [name=" + name + ", price=" + price + "]";
     }
 
     @Override
     public int compareTo(Product o) {
-        return price.compareTo(o.getPrice());
+        return name.toUpperCase().compareTo(o.getName().toUpperCase());
     }
 
     @Override
